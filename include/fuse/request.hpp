e6d2c95361a6d10a0736b13760803dfc71f49207
Jonas Schäfer <j.wielicki@sotecware.net>
2019-11-10 15:14:07 +0100

    Add layer of indirection for Fuse::Request methods
    This will be helpful to later create unit tests which test the
    actual fuse filesystem without having the entire fuse stack
    inbetween.
    
    Having the entire fuse stack inbetween would also cause lots of
    interference from other processes which scan freshly mounted
    filesystems and stuff like that.

--- include/fuse/request.hpp
+++ include/fuse/request.hpp
@@ -12,6 +12,40 @@
 
 namespace Fuse {
 
+struct RequestBackend {
+    void *(*req_userdata)(fuse_req_t);
+    const fuse_ctx*(*req_ctx)(fuse_req_t);
+    int (*req_getgroups)(fuse_req_t, int, gid_t*);
+    void (*req_interrupt_func)(fuse_req_t, fuse_interrupt_func_t, void*);
+    int (*req_interrupted)(fuse_req_t);
+
+    void (*reply_none)(fuse_req_t);
+    int (*reply_err)(fuse_req_t, int);
+    int (*reply_entry)(fuse_req_t, const fuse_entry_param*);
+    int (*reply_create)(fuse_req_t, const fuse_entry_param*,
+                        const fuse_file_info*);
+    int (*reply_attr)(fuse_req_t, const struct stat*, double);
+    int (*reply_readlink)(fuse_req_t, const char*);
+    int (*reply_open)(fuse_req_t, const fuse_file_info*);
+    int (*reply_write)(fuse_req_t, size_t);
+    int (*reply_buf)(fuse_req_t, const char*, size_t);
+    int (*reply_data)(fuse_req_t, struct fuse_bufvec*,
+                      enum fuse_buf_copy_flags);
+    int (*reply_iov)(fuse_req_t, const iovec*, int);
+    int (*reply_statfs)(fuse_req_t, const struct statvfs*);
+    int (*reply_xattr)(fuse_req_t, size_t);
+    int (*reply_lock)(fuse_req_t, const flock*);
+    int (*reply_bmap)(fuse_req_t, uint64_t idx);
+    int (*reply_ioctl_retry)(fuse_req_t,
+                             const iovec*, size_t,
+                             const iovec*, size_t);
+    int (*reply_ioctl)(fuse_req_t, int, const void*, size_t);
+    int (*reply_ioctl_iov)(fuse_req_t, int, const iovec*, int);
+    int (*reply_poll)(fuse_req_t, unsigned);
+};
+
+extern RequestBackend backend;
+
 class Request {
 public:
     Request();
@@ -65,75 +99,75 @@ public:
 
 public: /* GETTERS */
     inline void *userdata() {
-        return fuse_req_userdata(m_req);
+        return backend.req_userdata(m_req);
     }
 
     inline const fuse_ctx *ctx() {
-        return fuse_req_ctx(m_req);
+        return backend.req_ctx(m_req);
     }
 
     inline int getgroups(int size, gid_t *list) {
-        return fuse_req_getgroups(m_req, size, list);
+        return backend.req_getgroups(m_req, size, list);
     }
 
     inline void interrupt_func(fuse_interrupt_func_t func,
                                void *data)
     {
-        return fuse_req_interrupt_func(m_req, func, data);
+        return backend.req_interrupt_func(m_req, func, data);
     }
 
     inline int interrupted() {
-        return fuse_req_interrupted(m_req);
+        return backend.req_interrupted(m_req);
     }
 
 public: /* REPLY FUNCTIONS */
     inline void reply_none() {
-        fuse_reply_none(release());
+        backend.reply_none(release());
     }
 
     inline int reply_err(int err)
     {
         check();
-        return fuse_reply_err(release(), err);
+        return backend.reply_err(release(), err);
     }
 
     inline int reply_entry(const fuse_entry_param *e)
     {
         check();
-        return fuse_reply_entry(release(), e);
+        return backend.reply_entry(release(), e);
     }
 
     inline int reply_create(const fuse_entry_param *e,
                             const fuse_file_info *fi)
     {
         check();
-        return fuse_reply_create(release(), e, fi);
+        return backend.reply_create(release(), e, fi);
     }
 
     inline int reply_attr(const struct stat &attr, double attr_timeout)
     {
         check();
-        return fuse_reply_attr(release(), &attr, attr_timeout);
+        return backend.reply_attr(release(), &attr, attr_timeout);
     }
 
     inline int reply_readlink(const char *link) {
         check();
-        return fuse_reply_readlink(release(), link);
+        return backend.reply_readlink(release(), link);
     }
 
     inline int reply_open(const fuse_file_info *fi) {
         check();
-        return fuse_reply_open(release(), fi);
+        return backend.reply_open(release(), fi);
     }
 
     inline int reply_write(size_t count) {
         check();
-        return fuse_reply_write(release(), count);
+        return backend.reply_write(release(), count);
     }
 
     inline int reply_buf(const char *buf, size_t size) {
         check();
-        return fuse_reply_buf(release(), buf, size);
+        return backend.reply_buf(release(), buf, size);
     }
 
     inline int reply_buf(const std::basic_string<char> &s) {
@@ -143,55 +177,55 @@ public: /* REPLY FUNCTIONS */
 
     inline int reply_data(struct fuse_bufvec *bufv, enum fuse_buf_copy_flags flags) {
         check();
-        return fuse_reply_data(release(), bufv, flags);
+        return backend.reply_data(release(), bufv, flags);
     }
 
     inline int reply_iov(const iovec *iov, int count) {
         check();
-        return fuse_reply_iov(release(), iov, count);
+        return backend.reply_iov(release(), iov, count);
     }
 
     inline int reply_statfs(const struct statvfs *stbuf) {
         check();
-        return fuse_reply_statfs(release(), stbuf);
+        return backend.reply_statfs(release(), stbuf);
     }
 
     inline int reply_xattr(size_t count) {
         check();
-        return fuse_reply_xattr(release(), count);
+        return backend.reply_xattr(release(), count);
     }
 
     inline int reply_lock(const flock *lock) {
         check();
-        return fuse_reply_lock(release(), lock);
+        return backend.reply_lock(release(), lock);
     }
 
     inline int reply_bmap(uint64_t idx) {
         check();
-        return fuse_reply_bmap(release(), idx);
+        return backend.reply_bmap(release(), idx);
     }
 
     inline int reply_ioctl_retry(const iovec *in_iov, size_t in_count,
                                  const iovec *out_iov, size_t out_count) {
         check();
-        return fuse_reply_ioctl_retry(release(),
-                                      in_iov, in_count,
-                                      out_iov, out_count);
+        return backend.reply_ioctl_retry(release(),
+                                         in_iov, in_count,
+                                         out_iov, out_count);
     }
 
     inline int reply_ioctl(int result, const void *buf, size_t size) {
         check();
-        return fuse_reply_ioctl(release(), result, buf, size);
+        return backend.reply_ioctl(release(), result, buf, size);
     }
 
     inline int reply_ioctl_iov(int result, const iovec *iov, int count) {
         check();
-        return fuse_reply_ioctl_iov(release(), result, iov, count);
+        return backend.reply_ioctl_iov(release(), result, iov, count);
     }
 
     inline int reply_poll(unsigned revents) {
         check();
-        return fuse_reply_poll(release(), revents);
+        return backend.reply_poll(release(), revents);
     }
 };
 
--- src/fuse/request.cpp
+++ src/fuse/request.cpp
@@ -2,6 +2,35 @@
 
 namespace Fuse {
 
+RequestBackend backend{
+    .req_userdata = &fuse_req_userdata,
+    .req_ctx = &fuse_req_ctx,
+    .req_getgroups = &fuse_req_getgroups,
+    .req_interrupt_func = &fuse_req_interrupt_func,
+    .req_interrupted = &fuse_req_interrupted,
+
+    .reply_none = &fuse_reply_none,
+    .reply_err = &fuse_reply_err,
+    .reply_entry = &fuse_reply_entry,
+    .reply_create = &fuse_reply_create,
+    .reply_attr = &fuse_reply_attr,
+    .reply_readlink = &fuse_reply_readlink,
+    .reply_open = &fuse_reply_open,
+    .reply_write = &fuse_reply_write,
+    .reply_buf = &fuse_reply_buf,
+    .reply_data = &fuse_reply_data,
+    .reply_iov = &fuse_reply_iov,
+    .reply_statfs = &fuse_reply_statfs,
+    .reply_xattr = &fuse_reply_xattr,
+    .reply_lock = &fuse_reply_lock,
+    .reply_bmap = &fuse_reply_bmap,
+    .reply_ioctl_retry = &fuse_reply_ioctl_retry,
+    .reply_ioctl = &fuse_reply_ioctl,
+    .reply_ioctl_iov = &fuse_reply_ioctl_iov,
+    .reply_poll = &fuse_reply_poll,
+};
+
+
 Request::Request():
     m_req(nullptr),
     m_default_error(ECANCELED)
